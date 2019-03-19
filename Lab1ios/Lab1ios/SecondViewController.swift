d7cff1e2fe8e8da653a556b9fcafaaddaccbfbd6
Ірина Цьона <irunkatsyona@gmail.com>
2019-03-19 11:02:56 +0200

    Initial commit

---
+++ Lab1ios/Lab1ios/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Lab1ios
+//
+//  Created by Богдан Дацьків on 3/19/19.
+//  Copyright © 2019 Ірина Цьона. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
